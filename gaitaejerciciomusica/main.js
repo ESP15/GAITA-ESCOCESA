3d61a5111ac99580a253c0af2145b0379e256ab3
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-05-02 12:56:58 +0200

    Add files via upload

---
+++ gaitaejerciciomusica/main.js
@@ -0,0 +1,18 @@
+/*AUDIOS EN LAS IMÁGENES*/
+
+document.getElementById('Rangers').addEventListener('click', function() {
+    var audio = document.getElementById('gaita1');
+    audio.play();
+});
+document.getElementById('Celtic').addEventListener('click', function() {
+    var audio = document.getElementById('gaita1');
+    audio.play();
+});
+document.getElementById('Dundee').addEventListener('click', function() {
+    var audio = document.getElementById('gaita1');
+    audio.play();
+});
+document.getElementById('Hibernian').addEventListener('click', function() {
+    var audio = document.getElementById('gaita1');
+    audio.play();
+});
